8c716abcce2b88da3e7f6a97aaff6e0fd3123d1d
caio10012 <caiofontes183@gmail.com>
2026-03-30 18:02:11 -0300

    feat: habilitar suporte a touch e implementar pulo do player

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,6 +6,11 @@ const JUMP_VELOCITY := -600.0
 var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
+func _input(event: InputEvent) -> void:
+	if event is InputEventScreenTouch and event.pressed and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += gravity * delta
