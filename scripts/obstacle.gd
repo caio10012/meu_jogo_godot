399208a84d3a7510f58e159dc74d4598bd589727
caio10012 <caiofontes183@gmail.com>
2026-03-30 18:18:43 -0300

    fix: usar call_deferred para evitar erro de física no game over

--- scripts/obstacle.gd
+++ scripts/obstacle.gd
@@ -15,3 +15,4 @@ func _process(delta: float) -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		print("Game Over")
+		get_tree().call_deferred("reload_current_scene")
